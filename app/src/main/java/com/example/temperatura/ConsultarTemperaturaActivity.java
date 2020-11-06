7f63fd772f7ebe1f6b1c6b78d6a6283b08b3a994
Hugo <hugofigueira0@gmail.com>
2020-11-06 20:53:06 +0000

    Casas decimais

--- app/src/main/java/com/example/temperatura/ConsultarTemperaturaActivity.java
+++ app/src/main/java/com/example/temperatura/ConsultarTemperaturaActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class ConsultarTemperaturaActivity extends AppCompatActivity {
 
     @Override
@@ -13,13 +15,15 @@ public class ConsultarTemperaturaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_temperatura);
 
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
        /* Intent intent = getIntent();
         double temperatura = intent.getDoubleExtra("TEMPERATURA", 0.0);
 */
         TextView textViewCelsius = findViewById(R.id.textViewTemperaturaC);
-        textViewCelsius.setText(String.valueOf(DadosApp.temperatura.GetCelsius()));
+        textViewCelsius.setText(decimalFormat.format(DadosApp.temperatura.GetCelsius()));
 
         TextView textViewFahrenheit = findViewById(R.id.textViewTemperaturaF);
-        textViewFahrenheit.setText(String.valueOf(DadosApp.temperatura.GetFahrenheit()));
+        textViewFahrenheit.setText(decimalFormat.format(DadosApp.temperatura.GetFahrenheit()));
     }
 }
