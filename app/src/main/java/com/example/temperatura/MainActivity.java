82e14888d9895a2f2778a604f63ff0715656af88
Hugo <hugofigueira0@gmail.com>
2020-11-06 20:45:40 +0000

    Validação

--- app/src/main/java/com/example/temperatura/MainActivity.java
+++ app/src/main/java/com/example/temperatura/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //public static  final String TEMPERATURA = "pt.ipg.farenheit.TEMPERATURA";
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         RadioButton radioButtonCelcius = findViewById(R.id.radioButtonCelsius);
 
+        RadioButton radioButtonFahrenheit = findViewById(R.id.radioButtonFah);
 
 
         double temperatura;
@@ -32,13 +35,22 @@ public class MainActivity extends AppCompatActivity {
         }catch (NumberFormatException e){
 
             e.printStackTrace();
+            editText.setError("Introduza uma temperatura valida");
+            editText.requestFocus();
             return;
         }
 
         if(radioButtonCelcius.isChecked()) {
             DadosApp.temperatura = new TemperaturaCelsius(temperatura);
-        }else {
+        }else if(radioButtonFahrenheit.isChecked()) {
             DadosApp.temperatura = new TemperaturaFahrenheit(temperatura);
+        }else{
+            radioButtonCelcius.setError("Indique as unidades da temperadtura");
+            radioButtonFahrenheit.setError("Indique as unidades da temperadtura");
+
+            Toast.makeText(this, "Indique as unidades da temperadtura", Toast.LENGTH_LONG).show();
+            return;
+
         }
 
         Intent intent = new Intent(this, ConsultarTemperaturaActivity.class);
